25d8e01e0aa69fae81d98223e5c67d4ceeaecfd3
Julien Lussiez <jlussiez.la-providence.net>
2020-11-03 15:13:58 +0100

    Ex

---
+++ PhP/Ex8/Ex8.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$_SESSION['nom'] = 0;
+$_SESSION['age'] = 0;
+
+echo('
+
+<form action="Formulaire.php" method="get">
+
+ <p>Votre nom : <input type="text" name="login" /></p>
+ <p>Votre âge : <input type="text" name="mdp" /></p>
+
+ <p><input type="submit" value="Se connecter"></p>
+
+</form> ');
+
+highlight_file(__FILE__);
+?>
