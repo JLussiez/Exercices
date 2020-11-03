f31dc118bc49b460f44863a0d7747ea325f936e6
Julien Lussiez <jlussiez.la-providence.net>
2020-11-03 17:42:31 +0100

    PhP

--- PhP/TP1/Ex Final/ExFinal.php
+++ PhP/TP1/Ex Final/ExFinal.php
@@ -1,24 +1,32 @@
 <?php
-session_start();
-$_SESSION['nom'] = 0;
-$_SESSION['age'] = 0;
-
-echo('
-
-<form action="Formulaire.php" method="get">
-
- <p>Votre nom : <input type="text" name="login" /></p>
- <p>Votre âge : <input type="text" name="mdp" /></p>
-
-</form> ');
-
-echo (' <form action=session_destroy();> <p><input type="submit" value="OK"></p>');
-
-if (session_destroy()) {
-    echo 'Session détruite !';
-} else {
-    echo 'Erreur : impossible de détruire la session !';
-}
-
-
-highlight_file(__FILE__);
+  define('LOGIN','1234');
+  define('PASSWORD','julien');
+  $errorMessage = '';
+ 
+  if(!empty($_POST)) 
+  {
+    if(!empty($_POST['login']) && !empty($_POST['mdp'])) 
+    {
+      if($_POST['login'] !== LOGIN) 
+      {
+        $errorMessage = 'Mauvais login !';
+      }
+        elseif($_POST['mdp'] !== PASSWORD) 
+      {  
+        $errorMessage = 'Mauvais mot de passe !';
+      }
+        else
+      {
+        session_start();
+        $_SESSION['login'] = LOGIN;
+        header('Location: http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex%20Final/ExFinal.php');
+        exit();
+      }
+    }
+      else
+    {
+      $errorMessage = 'Veuillez saisir correctement vos identifiants !';
+    }
+  }
+?>
+https://www.apprendre-php.com/tutoriels/tutoriel-14-les-sessions.html
