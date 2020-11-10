326b7d2ee2680968a890901495a7b1a4bf88f6ad
Julien Lussiez <jlussiez.la-providence.net>
2020-11-10 14:53:35 +0100

    ExFinalTP1

--- PhP/TP1/Ex Final/ExFinal.php
+++ PhP/TP1/Ex Final/ExFinal.php
@@ -1,32 +1,67 @@
-<?php
-  define('LOGIN','1234');
-  define('PASSWORD','julien');
-  $errorMessage = '';
- 
-  if(!empty($_POST)) 
-  {
-    if(!empty($_POST['login']) && !empty($_POST['mdp'])) 
-    {
-      if($_POST['login'] !== LOGIN) 
-      {
-        $errorMessage = 'Mauvais login !';
-      }
-        elseif($_POST['mdp'] !== PASSWORD) 
-      {  
-        $errorMessage = 'Mauvais mot de passe !';
-      }
-        else
-      {
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <?php
         session_start();
-        $_SESSION['login'] = LOGIN;
-        header('Location: http://192.168.65.60/julien/TPWEB/PhP/TP1/Ex%20Final/ExFinal.php');
-        exit();
-      }
-    }
-      else
-    {
-      $errorMessage = 'Veuillez saisir correctement vos identifiants !';
-    }
-  }
-?>
-https://www.apprendre-php.com/tutoriels/tutoriel-14-les-sessions.html
+    ?>
+        <?php
+            
+            if(isset($_SESSION['log']) && $_SESSION['log'])
+            {
+                
+                echo "<h1>Bienvenue " .$_SESSION['Login']. "</h1>
+                    <form action='' method='post'>
+                        <div>
+                            <button type='submit' name='Logout'>Se déconnecter</button>
+                        </div>
+                    </form>";
+                if(isset($_POST['Logout']))
+                {
+                    session_destroy();
+                    header("refresh:0");
+                }
+            
+            }
+            else{
+                echo"<form action='' method='post'>
+                        <div>
+                            <label for='Login'>Login : </label>
+                            <input type='text' name='Login' id='Login'>
+                        </div>
+                        <div>
+                            <label for='MDP'>Mot de passe : </label>
+                            <input type='password' name='MDP' id='MDP'>
+                        </div>
+                        <div>
+                            <button type='submit' name='LoginButton'>Se connecter</button>
+                        </div>
+                    </form>";
+            
+
+                if (isset($_POST['LoginButton'])) {
+                    if($_POST['Login'] =='Julien'){
+                        $_SESSION['Login']=$_POST['Login'];
+                        if($_POST['MDP']=='1234'){
+                            $_SESSION['MDP']=$_POST['MDP'];
+                            $_SESSION['log']= true;
+                            header("refresh:0");
+                        }else{
+                            echo"Mot de passe inconnu";
+                        }
+                    }else{
+                        echo"Login inconnu";
+                    }
+                }
+            }
+            highlight_file(__FILE__);
+        ?>
+
+</body>
+
+</html>
